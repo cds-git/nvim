dc5baf4a122e62f5952ed6ad1ec0bc271ff70cc0
cds <due179@gmail.com>
2024-05-11 23:10:08 +0200

    Add gitsigns and vim-fugitive

---
+++ lua/plugins/git-config.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { desc = "preview git hunk" })
+			vim.keymap.set(
+				"n",
+				"<leader>gt",
+				"<cmd>Gitsigns toggle_current_line_blame<CR>",
+				{ desc = "toggle blame line" }
+			)
+		end,
+	},
+}
