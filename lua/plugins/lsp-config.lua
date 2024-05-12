77cb1e043c7309bf287bfebaf8c994ee3ca35541
cds <due179@gmail.com>
2024-05-12 20:33:00 +0200

    Fix LSP and completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,67 +1,177 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		--lazy = false,
-		opts = {
-			ensure_installed = {
-				"stylua",
-				"prettierd",
-				"eslint_d",
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup(opts)
-		end,
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "folke/neodev.nvim", opts = {} },
 	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		--lazy = false,
-		opts = {
-			auto_install = true,
-			ensure_installed = {
-				"lua_ls",
-				"html",
-				"cssls",
-				"angularls",
-				"tsserver",
-				"omnisharp",
-			},
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		--lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	config = function()
+		-- import lspconfig plugin
+		local lspconfig = require("lspconfig")
 
-			local lspconfig = require("lspconfig")
+		-- import mason_lspconfig plugin
+		local mason_lspconfig = require("mason-lspconfig")
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
+		-- import cmp-nvim-lsp plugin
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
+		local keymap = vim.keymap
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
+			callback = function(ev)
+				-- Buffer local mappings.
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				local opts = { buffer = ev.buf, silent = true }
 
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.angularls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.omnisharp.setup({
-				capabilities = capabilities,
-			})
+				--local map = function(keys, func, desc)
+				--vim.keymap.set("n", keys, func, { buffer = ev.buf, desc = desc })
+				--end
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+				--local telescope = require("telescope.builtin")
+				--map("gd", telescope.lsp_definitions, "goto definition")
+				--map("gr", telescope.lsp_references, "show LSP references")
+				--
+
+				opts.desc = "Workspace symbols"
+				keymap.set("n", "<leader>ws", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
+
+				opts.desc = "Document symbols"
+				keymap.set("n", "<leader>ds", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+
+				opts.desc = "Show signature help"
+				keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts)
+
+				opts.desc = "Add workspace folder"
+				keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+
+				opts.desc = "Remove workspace folder"
+				keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+
+				opts.desc = "List workspace folders"
+				keymap.set("n", "<leader>wl", function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, opts)
+
+				-- set keybinds
+				opts.desc = "Show LSP references"
+				keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+
+				opts.desc = "Go to declaration"
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+				opts.desc = "Show LSP definitions"
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+				opts.desc = "Show LSP implementations"
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+				opts.desc = "Show LSP type definitions"
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+				opts.desc = "See available code actions"
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+				opts.desc = "Smart rename"
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+				opts.desc = "Show buffer diagnostics"
+				keymap.set("n", "<leader>ld", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+				opts.desc = "Show line diagnostics"
+				keymap.set("n", "<leader>lf", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+				opts.desc = "Go to previous diagnostic"
+				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+				opts.desc = "Go to next diagnostic"
+				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+				opts.desc = "Show documentation for what is under cursor"
+				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+				opts.desc = "Restart LSP"
+				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+			end,
+		})
+
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		-- (not in youtube nvim video)
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		mason_lspconfig.setup_handlers({
+			-- default handler for installed servers
+			function(server_name)
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+			["omnisharp"] = function()
+				-- configure omnisharp for C#/.NET
+				lspconfig.omnisharp.setup({
+					capabilities = capabilities,
+					cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+					-- Enables support for reading code style, naming convention and analyzer
+					-- settings from .editorconfig.
+					enable_editorconfig_support = true,
+					-- If true, MSBuild project system will only load projects for files that
+					-- were opened in the editor. This setting is useful for big C# codebases
+					-- and allows for faster initialization of code navigation features only
+					-- for projects that are relevant to code that is being edited. With this
+					-- setting enabled OmniSharp may load fewer projects and may thus display
+					-- incomplete reference lists for symbols.
+					enable_ms_build_load_projects_on_demand = false,
+					-- Enables support for roslyn analyzers, code fixes and rulesets.
+					enable_roslyn_analyzers = true,
+					-- Specifies whether 'using' directives should be grouped and sorted during
+					-- document formatting.
+					organize_imports_on_format = true,
+					-- Enables support for showing unimported types and unimported extension
+					-- methods in completion lists. When committed, the appropriate using
+					-- directive will be added at the top of the current file. This option can
+					-- have a negative impact on initial completion responsiveness,
+					-- particularly for the first few completion sessions after opening a
+					-- solution.
+					enable_import_completion = true,
+					-- Specifies whether to include preview versions of the .NET SDK when
+					-- determining which version to use for project loading.
+					sdk_include_prereleases = true,
+					-- Only run analyzers against open files when 'enableRoslynAnalyzers' is true
+					analyze_open_documents_only = false,
+				})
+			end,
+			["angularls"] = function()
+				lspconfig.angularls.setup({
+					capabilities = capabilities,
+					filetypes = { "angular", "html", "typescript", "typescriptreact" },
+					root_dir = lspconfig.util.root_pattern("angular.json", "project.json"),
+				})
+			end,
+			["lua_ls"] = function()
+				-- configure lua server (with special settings)
+				lspconfig["lua_ls"].setup({
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							-- make the language server recognize "vim" global
+							diagnostics = {
+								globals = { "vim" },
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				})
+			end,
+		})
+	end,
 }
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,50 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		-- used for installing formatters and linters automatically
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"omnisharp",
+				"tsserver",
+				"html",
+				"cssls",
+				"angularls",
+			},
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"prettierd", -- js/ts formatter
+				"eslint_d", -- js/ts linter
+				"shfmt", -- bash formatter
+				"vale", -- markdown formatter
+			},
+		})
+	end,
+}
