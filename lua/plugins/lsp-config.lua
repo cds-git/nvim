447cc9c984a19371bb287449fed0c72abcba4ec7
cds <due179@gmail.com>
2024-05-12 21:50:27 +0200

    Update LSP keymaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -25,74 +25,46 @@ return {
 				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
 
-				--local map = function(keys, func, desc)
-				--vim.keymap.set("n", keys, func, { buffer = ev.buf, desc = desc })
-				--end
+				local map = function(modes, keys, func, desc)
+					vim.keymap.set(modes, keys, func, { buffer = ev.buf, desc = desc })
+				end
 
-				--local telescope = require("telescope.builtin")
-				--map("gd", telescope.lsp_definitions, "goto definition")
-				--map("gr", telescope.lsp_references, "show LSP references")
-				--
+				local telescope = require("telescope.builtin")
 
-				opts.desc = "Workspace symbols"
-				keymap.set("n", "<leader>ws", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", opts)
+				map("n", "[d", vim.diagnostic.goto_prev, "Go to previous diagnostic")
+				map("n", "]d", vim.diagnostic.goto_next, "Go to next diagnostic")
 
-				opts.desc = "Document symbols"
-				keymap.set("n", "<leader>ds", "<cmd>Telescope lsp_document_symbols<CR>", opts)
+				-- common
+				map("n", "gr", telescope.lsp_references, "Show LSP references")
+				map("n", "gd", telescope.lsp_definitions, "Show LSP definitions")
+				map("n", "gi", telescope.lsp_implementations, "Show LSP implementations")
 
-				opts.desc = "Show signature help"
-				keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help, opts)
+				map("n", "K", vim.lsp.buf.hover, "Show documentation for what is under cursor")
+				map("n", "<leader>lf", vim.diagnostic.open_float, "Show line diagnostics")
 
-				opts.desc = "Add workspace folder"
-				keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+				map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "See available code actions")
+				map("n", "<leader>rn", vim.lsp.buf.rename, "Smart rename")
 
-				opts.desc = "Remove workspace folder"
-				keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+				map("n", "<leader>sh", vim.lsp.buf.signature_help, "Show signature help")
 
-				opts.desc = "List workspace folders"
-				keymap.set("n", "<leader>wl", function()
+				map("n", "<leader>ds", telescope.lsp_document_symbols, "Document symbols")
+
+				map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+				map("n", "gt", telescope.lsp_type_definitions, "Show LSP type definitions")
+
+				map("n", "<leader>ld", function()
+					telescope.diagnostics({ bufnr = 0 })
+				end, "Show buffer diagnostics")
+
+				map("n", "<leader>rs", ":LspRestart<CR>", "Restart LSP")
+
+				-- workspace
+				map("n", "<leader>ws", telescope.lsp_dynamic_workspace_symbols, "Workspace symbols")
+				map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, "Add workspace folder")
+				map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, "Remove workspace folder")
+				map("n", "<leader>wl", function()
 					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				end, opts)
-
-				-- set keybinds
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
-
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>ld", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>lf", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				end, "List workspace folders")
 			end,
 		})
 
