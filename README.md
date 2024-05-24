1a1917c21dbebf971b03a59e09ba78b4a26ecefe
cds <due179@gmail.com>
2024-05-24 22:17:12 +0200

    Add icons to debugging and enable launching debugging session in dotnet directly from nvim

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## TODO
 
 * Integrated terminal, maybe toggleterm
+    * Add window with lazygit
 * Way to navigate quickly between files, maybe grapple or harpoon
 * Maybe install oil for editing system files
 * Add keybindings for splitting screen and navigation
@@ -10,4 +11,4 @@
 * Fix lualine to not split in middle for other windows
 * Make better integration for trouble
 * Fix neotest for dotnet, after it somehow broke
-* Add icons to debugging
+* Create a new theme or port bearded arc to nvim
--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -47,30 +47,56 @@ return {
 	config = function()
 		local dap = require("dap")
 
-		require("dap").adapters["coreclr"] = {
+		dap.adapters.coreclr = {
 			type = "executable",
 			command = "netcoredbg",
 			args = { "--interpreter=vscode" },
 		}
 
-		dap.configurations["cs"] = {
+		dap.configurations.cs = {
 			{
 				type = "coreclr",
 				name = "Attach",
 				request = "attach",
 				processId = require("dap.utils").pick_process,
 			},
-			-- {
-			-- 	type = "coreclr",
-			-- 	name = "Launch file",
-			-- 	request = "launch",
-			-- 	---@diagnostic disable-next-line: redundant-parameter
-			-- 	program = function()
-			-- 		return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
-			-- 	end,
-			-- 	cwd = "${workspaceFolder}",
-			-- },
+			{
+				type = "coreclr",
+				name = "Launch - netcoredbg",
+				request = "launch",
+				console = "integratedTerminal",
+				program = function()
+					if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+						require("utils").dotnet_build_project()
+					end
+					return require("utils").dotnet_get_dll_path()
+				end,
+			},
 		}
+		vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+		vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+		vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+		vim.fn.sign_define(
+			"DapBreakpoint",
+			{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointCondition",
+			{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapBreakpointRejected",
+			{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+		)
+		vim.fn.sign_define(
+			"DapLogPoint",
+			{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+		)
+		vim.fn.sign_define(
+			"DapStopped",
+			{ text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+		)
 	end,
 	keys = {
 		{
