7afc3b2f8aff50829974e10d113929961bbe4ef9
cds-git <due179@gmail.com>
2024-06-10 19:51:33 +0200

    Add tabbing to select through completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -41,6 +41,28 @@ return {
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						vim.fn.feedkeys(
+							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+							""
+						)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif require("luasnip").jumpable(-1) then
+						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 
 			-- sources for autocompletion
