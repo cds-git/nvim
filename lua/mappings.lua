03d16cf4d32cb47c8806e2e0991f55d59e3bc8c3
cds-git <due179@gmail.com>
2024-06-12 17:30:29 +0200

    Enable mapping for resizing windows

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,12 +6,12 @@ map("n", "<leader>ln", "<cmd>set nu!<CR>", { desc = "Toggle line number" })
 map("n", "<leader>lrn", "<cmd>set rnu!<CR>", { desc = "Toggle relative number" })
 
 -- Navigation while in insert mode
-map("i", "<C-b>", "<ESC>^i", { desc = "Move beginning of line" })
-map("i", "<C-e>", "<End>", { desc = "Move end of line" })
-map("i", "<C-h>", "<Left>", { desc = "Move left" })
-map("i", "<C-l>", "<Right>", { desc = "Move right" })
-map("i", "<C-j>", "<Down>", { desc = "Move down" })
-map("i", "<C-k>", "<Up>", { desc = "Move up" })
+-- map("i", "<C-b>", "<ESC>^i", { desc = "Move beginning of line" })
+-- map("i", "<C-e>", "<End>", { desc = "Move end of line" })
+-- map("i", "<C-h>", "<Left>", { desc = "Move left" })
+-- map("i", "<C-l>", "<Right>", { desc = "Move right" })
+-- map("i", "<C-j>", "<Down>", { desc = "Move down" })
+-- map("i", "<C-k>", "<Up>", { desc = "Move up" })
 
 -- File mappings
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "File save" })
@@ -31,22 +31,22 @@ map("n", "<leader>Y", '"+Y', { desc = "Copy to clipboard" })
 map({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete to void register" })
 
 -- Buffer navigation
-map("n", "<leader>[", ":bp!<CR>", { desc = "Previous buffer" })
-map("n", "<leader>]", ":bn!<CR>", { desc = "Next buffer" })
+map("n", "[b", ":bp!<CR>", { desc = "Previous buffer" })
+map("n", "]b", ":bn!<CR>", { desc = "Next buffer" })
 
 -- Window splitting
 map("n", "<leader>ss", ":split<Return><C-w>w", { desc = "[Window] Horisontal Split" })
 map("n", "<leader>sv", ":vsplit<Return><C-w>w", { desc = "[Window] Vertical Split" })
 map("n", "<leader>q", "<C-w>q", { desc = "[Window] Quit" })
 
--- Resize window using <ctrl> arrow keys
--- map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
--- map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
--- map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
--- map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
 -- Window navigations
 map("n", "<C-h>", "<C-w>h", { desc = "[Window] Switch left" })
 map("n", "<C-l>", "<C-w>l", { desc = "[Window] Switch right" })
 map("n", "<C-j>", "<C-w>j", { desc = "[Window] Switch down" })
 map("n", "<C-k>", "<C-w>k", { desc = "[Window] Switch up" })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
