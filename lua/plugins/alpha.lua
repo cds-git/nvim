e155fd80e9dfa4169bd0360bcae2918e6e716fd9
cds <due179@gmail.com>
2024-06-16 21:14:45 +0200

    Update dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,12 +1,13 @@
 return {
 	"goolord/alpha-nvim",
+	event = "VimEnter",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
 
 	config = function()
 		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
+		local dashboard = require("alpha.themes.dashboard")
 
 		dashboard.section.header.val = {
 			[[                                                                       ]],
@@ -25,6 +26,28 @@ return {
 			[[                                                                       ]],
 			[[                                                                       ]],
 		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+			dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+			-- dashboard.button("c", "  Config", ":e ~/.config/nvim/init.vim<CR>"),
+			-- dashboard.button("s", "  Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+			dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+		}
+
+		-- local function footer()
+		-- 	return "Don't Stop Until You are Proud..."
+		-- end
+		--
+		-- dashboard.section.footer.val = footer()
+		--
+		-- dashboard.section.footer.opts.hl = "Type"
+		-- dashboard.section.header.opts.hl = "Include"
+		-- dashboard.section.buttons.opts.hl = "Keyword"
+
+		-- dashboard.opts.opts.noautocmd = true
 
 		alpha.setup(dashboard.opts)
 	end,
