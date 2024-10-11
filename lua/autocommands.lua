296f87bbca9ecbe9ade3ee2f9fa81a910e03ab64
cds <due179@gmail.com>
2024-10-11 23:51:58 +0200

    Add autocmd for handling json files as jsonc

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, {
 		vim.bo.filetype = "angular"
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufEnter" }, {
+	pattern = { "*.json" },
+	callback = function()
+		vim.bo.filetype = "jsonc"
+	end,
+})
