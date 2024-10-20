7956d401cf8681555c1b32ae5ec867fcec03d498
cds <due179@gmail.com>
2024-10-20 22:24:13 +0200

    Set Roslyn as csharp LSP again

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -22,7 +22,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- Navigation
 		-- map("n", "gd", vim.lsp.buf.definition, "Go to LSP definitions")
 		-- map("n", "gi", vim.lsp.buf.implementation, "Go to LSP implementations")
-        map("n", "gr", vim.lsp.buf.references, "Show LSP references")
+		map("n", "gr", vim.lsp.buf.references, "Show LSP references")
 		map("n", "gi", telescope.lsp_implementations, "Go to LSP implementations")
 		map("n", "gd", telescope.lsp_definitions, "Show LSP definitions")
 		-- map("n", "gr", telescope.lsp_references, "Show LSP references")
@@ -126,18 +126,39 @@ return {
 				},
 			},
 			{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-            -- enables omnisharp goto definition with decompliation
-            { "Hoffs/omnisharp-extended-lsp.nvim", enabled = true, lazy = true },
+			-- enables omnisharp goto definition with decompliation
+			{ "Hoffs/omnisharp-extended-lsp.nvim", enabled = false, lazy = true },
 			-- { "seblj/nvim-lsp-extras" },
 			{ -- Roslyn LSP for C#/.NET instead of omnisharp
 				"seblj/roslyn.nvim",
-				enabled = false,
-				ft = "cs",
+				enabled = true,
 				config = function()
 					require("roslyn").setup({
 						config = {
 							capabilities = require("utility.capabilities").capabilities,
 							on_attach = require("utility.on_attach").on_attach,
+							settings = {
+								["csharp|background_analysis"] = {
+									dotnet_compiler_diagnostics_scope = "fullSolution",
+								},
+								["csharp|inlay_hints"] = {
+									csharp_enable_inlay_hints_for_implicit_object_creation = true,
+									csharp_enable_inlay_hints_for_implicit_variable_types = true,
+									csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+									csharp_enable_inlay_hints_for_types = true,
+									dotnet_enable_inlay_hints_for_indexer_parameters = true,
+									dotnet_enable_inlay_hints_for_literal_parameters = true,
+									dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+									dotnet_enable_inlay_hints_for_other_parameters = true,
+									dotnet_enable_inlay_hints_for_parameters = true,
+									dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+									dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+									dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+								},
+								["csharp|code_lens"] = {
+									dotnet_enable_references_code_lens = true,
+								},
+							},
 						},
 					})
 				end,
