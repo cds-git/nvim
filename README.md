bf8d34417015362f99ad5e584fdd86bd620bd0e0
cds <due179@gmail.com>
2024-10-26 14:12:11 +0200

    Add folding

--- README.md
+++ README.md
@@ -6,7 +6,3 @@ Greatest neovim config known to mankind
 
 - Look into [flash.nvim](https://github.com/folke/flash.nvim) for some movement util
 - Look into [noice.nvim](https://github.com/folke/noice.nvim) for improved UI
-- Markdown
-    - Add [markdown-toc](https://github.com/jonschlinkert/markdown-toc)
-    - Folding
-    - outline
---
+++ lua/plugins/folding.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		config = function()
+			local ufo = require("ufo")
+
+			vim.o.foldcolumn = "1"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+			vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+			vim.keymap.set("n", "zp", function()
+				local winid = ufo.peekFoldedLinesUnderCursor()
+				if not winid then
+					vim.lsp.buf.hover()
+				end
+			end, { desc = "Peek fold" })
+
+			ufo.setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "lsp", "indent" }
+				end,
+			})
+		end,
+	},
+}
