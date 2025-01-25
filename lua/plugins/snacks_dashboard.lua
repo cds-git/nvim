ef101bbf7cb6842a9f8ba8befb4d019808976a6b
cds <due179@gmail.com>
2025-01-25 11:41:05 +0100

    Use snacks.nvim dashboard

---
+++ lua/plugins/snacks_dashboard.lua
@@ -0,0 +1,53 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			enabled = true,
+			preset = {
+				header = [[                                                                       
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+			                                                                       ]],
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{ icon = "󱌣 ", key = "m", desc = "Mason", action = ":Mason" },
+					{
+						icon = "󰒲 ",
+						key = "l",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+			},
+		}
+    }
+}
+
