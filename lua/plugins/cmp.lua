819b4467b794a36255bca3cc9944ebf84d55f828
cds <due179@gmail.com>
2025-03-01 17:10:59 +0100

    Update and fix blink cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,7 +17,7 @@ return {
 			nerd_font_variant = "normal",
 		},
 		completion = {
-			accept = { auto_brackets = { enabled = true } },
+			-- accept = { auto_brackets = { enabled = true } },
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 250,
@@ -75,19 +75,15 @@ return {
 			["<C-down>"] = { "scroll_documentation_down", "fallback" },
 		},
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
-			cmdline = function()
-				local type = vim.fn.getcmdtype()
-				-- Search forward and backward
-				if type == "/" or type == "?" then
-					return { "buffer" }
-				end
-				-- Commands
-				if type == ":" then
-					return { "cmdline" }
-				end
-				return {}
-			end,
+			default = { "lsp", "snippets", "buffer", "path" },
+		},
+		cmdline = {
+			completion = { menu = { auto_show = true } },
+			keymap = {
+				["<Tab>"] = { "show", "accept" },
+				["<Up>"] = { "select_prev" },
+                ["<Down>"] = { "select_next" },
+			},
 		},
 	},
 }
